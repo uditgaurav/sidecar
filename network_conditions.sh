72fea5172a444e6a3410228df8b5e2dcd697f1f6
Udit Gaurav <udit.gaurav@harness.io>
2024-06-28 16:59:12 -0700

    Update Code
    Signed-off-by: Udit Gaurav <udit.gaurav@harness.io>

--- network_conditions.sh
+++ network_conditions.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 # Default values
-NETWORK_LOSS=${NETWORK_LOSS:-10}
+NETWORK_MODE=${NETWORK_MODE:-loss}
+NETWORK_PACKET_LOSS_PERCENTAGE=${NETWORK_PACKET_LOSS_PERCENTAGE:-10}
+NETWORK_LATENCY=${NETWORK_LATENCY:-200}
 DURATION=${DURATION:-60}
 TARGET_CONTAINER_IMAGE=${TARGET_CONTAINER_IMAGE:-"your-app-image"}
 
@@ -13,15 +15,20 @@ TARGET_PID=$(pgrep -f "$TARGET_CONTAINER_IMAGE")
 
 # Apply network conditions if the PID is found
 if [ -n "$TARGET_PID" ]; then
-  nsenter -t $TARGET_PID -n tc qdisc add dev eth0 root netem loss ${NETWORK_LOSS}%
-  echo "Applied ${NETWORK_LOSS}% network loss for ${DURATION} seconds"
+  if [ "$NETWORK_MODE" = "latency" ]; then
+    nsenter -t $TARGET_PID -n tc qdisc add dev eth0 root netem delay ${NETWORK_LATENCY}ms
+    echo "Applied ${NETWORK_LATENCY}ms network latency for ${DURATION} seconds"
+  else
+    nsenter -t $TARGET_PID -n tc qdisc add dev eth0 root netem loss ${NETWORK_PACKET_LOSS_PERCENTAGE}%
+    echo "Applied ${NETWORK_PACKET_LOSS_PERCENTAGE}% network loss for ${DURATION} seconds"
+  fi
   
   # Sleep for the specified duration
   sleep ${DURATION}
 
   # Remove the network conditions after the duration
   nsenter -t $TARGET_PID -n tc qdisc del dev eth0 root netem
-  echo "Network loss removed after ${DURATION} seconds"
+  echo "Network conditions removed after ${DURATION} seconds"
 fi
 
 # Keep the sidecar container running
